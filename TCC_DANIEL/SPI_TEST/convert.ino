f8b0499b0144566f02e81a17eabbb0dd9f8e18b9
Vinícius Silva Silva <60355368+ss0vinicius@users.noreply.github.com>
2025-11-28 04:54:51 -0300

    Delete TCC_DANIEL directory

--- TCC_DANIEL/SPI_TEST/convert.ino
+++
@@ -1,9 +0,0 @@
-int convertHexToInt(char data){
-  String HEX_CONVERT = "0123456789ABCDEF";
-  int value_hex = HEX_CONVERT.indexOf(data);
-  return value_hex; 
-}
-
-String ValueIntVectByte(int data){
-  return String(data,BIN);
-}
